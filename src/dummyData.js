71dcb89e7b06c4326db2e03a21c4d7a3c2a07fbd
Trevor <trevorfehrman@gmail.com>
2018-11-03 02:31:42 -0700

    Recipe Dummy Data

---
+++ src/dummyData.js
@@ -0,0 +1,54 @@
+export default [
+	{
+		title: "Pub Burger",
+		price: "$18.00",
+		ingredients: ["chuck", "butter", "brioche buns", "worcesteshire"],
+		instructions:
+			"Cube the chuck and place on a sheet tray.  Place in the freezer for 40 minutes.  In a food processor, mince the chuck into pices about 16th of an inch in diameter.  Melt the butter and pour over the chilled meat so that it congeals and season with salt and pepper.  Toast the brioche buns, mix the burger sauce and assemble.",
+	},
+	{
+		title: "Dumplings",
+		price: "$11.00",
+		ingredients: [
+			"wonton wrappers",
+			"ground pork",
+			"green onion",
+			"tamarai",
+		],
+		instructions:
+			"Pour oil into a a wok over high heat.  When oil begins to shimmer sear the pork for 2 minutes.  Toss the chopped green onion for 10-30 seconds and remove to aplate.  Assemble the tamari sauce.  Holding a wrapper in one hand, spoon a tablespoon of the pork mixture into the wrapper and crimp shut.  Sear each dumpling in a hot pan and then steam for 8 minutes",
+	},
+	{
+		title: "Flank Steak Tacos",
+		price: "$13.00",
+		ingredients: ["flank steak", "tortitllas", "cillantro", "lime"],
+		instructions:
+			"Heat oil in a large skillet until whisps of smoke appear.  Remove the steak from the marinade and pat dry with paper towels.  Working in batches, sear one side of the steak for two minutes, then flips and sear for another two minutes.  Immediatly remove from the heat and repeat with remaining cuts.  Toast the tortialls over an open flame and roughly chop the cillanto.  Dice the steak, assemble tacos and serve.",
+	},
+	{
+		title: "Mozzarella Sticks ",
+		price: "$8.00",
+		ingredients: [
+			"mozzarella",
+			"flour",
+			"canolla oil",
+			"San Marzano tomoatoes",
+		],
+		instructions:
+			"Assmeble the flour, egg and breading into three separate plates.  Working one at a time, dip a mozzarella plank into the flour, egg and breading in that order and then carefully let rest on a wire rack set inside a rimmed baking sheet.  Pour the tomatoes into a small suacepan and cook down until the consistency is soft but still textured.  Add seasoning.  Heat canola oil to 440 degrees and carefull drop prepared sticks into the oil.  Cook for 60 seconds then remove to a paper tower lined plate.",
+	},
+	{
+		title: "Spicy Popcorn",
+		price: "3.00",
+		ingredients: ["popcorn", "cayanne", "garlic powder", "salt"],
+		instructions:
+			"Make a terrible mistake and have no food in the house.  Be hungry.  Rummage through you cupboards like a national park bear recently awoken from hibernation.  Gaze skeptically at a can of black beans.  Decide that's too complicated.  Open the fridge and wonder if you can eat slices off of your wedge of paresean cheese.  Is that thing people do?  Decide it isn't.  Make popcorn and realize too late you don't have butter.  Dust popcorn with cayenne pepper like you're dusting a fancy cake.  Complain incessentaly about how spicy the popcorn is even as you keep shoving mouthful after mouthful into your face.",
+	},
+	{
+		title: "Macarons",
+		price: "$12.00",
+		ingredients: ["egg whites, sugar, food coloring, almond flour"],
+		instructions:
+			"Make the firm meringue.  Whip eggwhites with a pinch of cream of tarter until stiff peaks have formed.  The acidity of the cream of tarter will help coagulate the albumin while preventing weeping.  Pipe firm merinuge into silver-dollar sized discs on a parchment lined baking sheet.  Bake for 15 minutes at 350 degrees.  Assemble the filling.  Mix butter, sugar and almond flour and beat until a smooth paste has formed.  Assemble and let stand in refigerator for 20 minutes.",
+	},
+];
