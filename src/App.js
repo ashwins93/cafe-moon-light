1cefb036a01e4dcd25a85c7d7e7c65e069533643
Ashwin <aswinjeganath@gmail.com>
2018-11-03 14:14:42 +0530

    Add primary header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header class="Header">
+      <div className="container">
+        <h1>
+          Cafe <br /> Moon Light
+        </h1>
+        <div className="bg-wrapper" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
